fd273920dd932d0138dcb9574735a24ecf83048c
Lucia <lubersol@gmail.com>
2020-11-05 21:31:21 +0100

    creacion relacion entre los modelos

--- models/user.js
+++ models/user.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Cita);
+      this.hasOne(models.Historial);
     }
   };
   User.init({
