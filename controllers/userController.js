d9d8a05b7267bfdfe869a7811e99f4578e40291d
Lucia <lubersol@gmail.com>
2020-11-20 17:25:37 +0100

    cambiado codigo token usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,19 @@ const {
     User
 } = require('../models');
 
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cita = require('../models/cita');
 //Importar el middleware auth
-const auth = require('../middlewares/auth');
+//const auth = require('../middlewares/auth');
 
 const UserController = {
     showAll(req, res) {
-        User.findAll()
+        User.findAll({
+            include:[{
+                model: Cita
+            }]
+        })
             .then(users => res.send(users))
             .catch(error => {
                 console.error(error);
@@ -24,8 +29,8 @@ const UserController = {
         const u = { nombre, apellidos, rol, email, password, covid, telefono, direccion, deudor, dni, dob } = req.body;
         User.create(u).then((user) => {
             res.status(200).json(
-                {message:'Usuario registrado correctamente'+ u}
-                );
+                { message: 'Usuario registrado correctamente' }
+            );
         }).catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -34,21 +39,51 @@ const UserController = {
 
     //Login usuario
     async login(req, res) {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).send(
-                'Faltan datos'
-            );
-        }
+        // const { email, password } = req.body;
+        // if (!email || !password) {
+        //     return res.status(400).send(
+        //         'Faltan datos'
+        //     );
+        // }
         try {
-            let user = await User.authenticate(email, password)
-            user = await user.authorize();
-            return res.json(user);
-        } catch (err) {
-            return res.status(400).json(
-                {message:'Datos introducidos incorrectos'});
+            const user = await User.findOne({
+                where:{
+                email: req.body.email
+            }
+            })
+            if(!user){
+                return res.status(400).send({
+                    message:'Datos incorrectos'
+                });
+            }
+            const token = jwt.sign({
+                id:user.id
+            }, 'gatopaseandoporelteclado',{
+                expiresIn:'1y'
+            })
+            res.send({
+                user,
+                token,
+                message: 'Has accedido correctamente'
+            });
+        }catch (error){
+            res.status(400).send({
+                message:'No has accedido correctamente'
+            });
         }
     },
+    //         user = await user.authorize();
+    //         return res.json({
+    //             user,
+    //             token,
+    //             message:'Has accedido correctamente'
+    //         });
+    //     } catch (err) {
+    //         return res.status(400).json(
+    //             { message: 'Datos introducidos incorrectos' }
+    //         );
+    //     }
+    // },
 
     //Logout usuario
     async logout(req, res) {
