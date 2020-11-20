e365990d69df3a69d498e51e9000c4f4a1f720c1
Lucia <lubersol@gmail.com>
2020-11-20 16:33:47 +0100

    cambiada ruta get general api

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function (req, res, next) { //para evitar el error CORS
 });
 
 // Ruta por defecto con mensaje de bienvenida en formato JSON.
-app.get('*', (req, res) => res.status(200).send({
+app.get('/', (req, res) => res.status(200).send({
     message: 'Bienvenido a nuestra clínica dental'
 }));
 
