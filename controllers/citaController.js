c8d2ec1c03ac8f1096cd1ad99b70cc3f2510f21c
Lucia <lubersol@gmail.com>
2020-11-20 22:43:06 +0100

    corregido error controller citas

--- controllers/citaController.js
+++ controllers/citaController.js
@@ -22,7 +22,7 @@ const CitaController = {
     //Ver citas pendientes
     showPending(req, res) {
         Cita.findAll({
-            where: { status: 'pending', userId: req.params.userId },
+            where: { status: 'pending', userId: req.params.userId }
         }).then(citas => {
             res.send(citas);
         }).catch(error => {
