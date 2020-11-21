b2a4734e48e39d3be622649c73b7d90d6ed5b068
Lucia <lubersol@gmail.com>
2020-11-21 12:06:30 +0100

    añadido Cors

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+//CORS
+const cors = require('cors');
+app.use(cors());
+
 //Importar mysql2
 const mysql = require('mysql2/promise');
 
